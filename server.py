e27ccbf3a001c2caa9a4844d27dc020a4fbc5989
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-06-25 15:14:54 +0700

    # feat add waitress and production server

--- server.py
+++ server.py
@@ -4,11 +4,12 @@ import numpy as np
 from flask_cors import CORS
 import pickle
 import pandas as pd
+from waitress import serve
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": [
     "http://localhost:3000", 
-    "https://peter-louisx.github.io"
+    "https://peter-louisx.github.io",
 ]}})
 
 model = pickle.load(open('model/model.pkl', 'rb'))
@@ -31,26 +32,19 @@ def predict():
         
         pred = model.predict(input_df)
 
-        target_labels = {
-            'Insufficient_Weight': 0,
-            'Normal_Weight': 1,
-            'Overweight_Level_I': 2,
-            'Overweight_Level_II': 3,
-            'Obesity_Type_I': 4,
-            'Obesity_Type_II': 5,
-            'Obesity_Type_III': 6,
-        }
-
-        for key, value in target_labels.items():
-            if value == pred[0]:
-                pred = key
-                break
+        target_labels = [
+            'Insufficient Weight',
+            'Normal Weight',
+            'Overweight Level I',
+            'Overweight Level II',
+            'Obesity Type I',
+            'Obesity Type II',
+            'Obesity Type III',
+        ]
         
-        return jsonify({'prediction': pred.replace("_", " ")})
+        return jsonify({'prediction': target_labels[pred]})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')  # Runs on http://127.0.0.1:5000 by default
-    #change host to 0.0.0.
-    print('hello world')
+    serve(app, host='0.0.0.0', port=5000)
